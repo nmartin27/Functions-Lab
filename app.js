930bb6d2722e6a0799130d591d963424400d910a
nmartin <nate.martin27@gmail.com>
2024-10-18 10:29:27 -0400

    Exercise 5

--- app.js
+++ app.js
@@ -82,3 +82,25 @@ return `${name}@${domain}`
 
 
 console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+const greetUser = (name, timeOfDay) => {
+    if (timeOfDay === "morning") {
+        return `Good morning, ${name}`;
+} else if (timeOfDay === "afternoon") {
+    return `Good Afternoon, ${name}`;
+} else if (timeOfDay === "evening") {
+return `Good Evening, ${name}`;
+}
+}
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
